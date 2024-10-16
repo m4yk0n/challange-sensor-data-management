c80cdcb0419190660d955410544f4882232030a9
m4yk0n <maykon.rodrigues@sptech.school>
2024-10-16 12:17:34 -0300

    Ajustes modal

--- src/pages/Modal.jsx
+++ src/pages/Modal.jsx
@@ -5,25 +5,25 @@ const Modal = ({ sensor, fecharModal }) => {
   return (
     <div className="modal-overlay">
       <div className="modal-content">
-        <h2>Detalhes do sensor {sensor.id}</h2>
-        <p>Média Temperatura: {sensor.mediaTemp}ºC</p>
-        <p>Status: {sensor.status}</p>
+        <h2 className="titulo-modal">Detalhes do sensor <b>{sensor.id}</b></h2>
+        <p className="indicador-modal">Média Temperatura: {sensor.mediaTemp}ºC</p>
+        <p className="indicador-modal">Status: {sensor.status}</p>
         
-        <h3>Leituras Detalhadas:</h3>
-        <ul>
+        <h3 className="subtitulo-modal">Lista de Leituras:</h3>
+        <ul className="conjunto-lista">
           {sensor.leituras.map((leitura, index) => {
             const data = new Date(leitura.data);
             const dataFormatada = `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()} - ${data.getHours()}:${data.getMinutes()}:${data.getSeconds()} (BRA)`;
             
             return (
-              <li key={index}>
+              <li key={index} className="lista-leituras">
                 {dataFormatada} - {leitura.temp}ºC
               </li>
             );
           })}
         </ul>
         
-        <button onClick={fecharModal}>Fechar</button>
+        <button className="botao-modal" onClick={fecharModal}>Fechar</button>
       </div>
     </div>
   );
